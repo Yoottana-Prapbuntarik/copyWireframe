d530b2ce54bdd13adf023c5daa17d5febc11452a
unknown <nap3831@gmail.com>
2019-08-23 20:33:02 +0700

    edit page order

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import Mycart from '../pages/Mycart';
+import axios from 'axios';
+import Cart from './Cart';
 class Order extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        }
+    }
+    componentDidMount() {
+        axios.get('https://shop-api-services.herokuapp.com/Cart').then((response) => {
+            this.setState({
+                data: response.data[0]
+            })
+        })
+    }
 
     render() {
         return (
             <div>
                 <h1>รายการสั่งซื้อ</h1>
-                <Mycart/>
+                <Cart  dataProduct={this.state.data}/>
             </div>
         )
     }
